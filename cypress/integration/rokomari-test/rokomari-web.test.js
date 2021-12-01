7071c908c2717951ddfcf27258729c6de2ded6b7
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-12-01 23:19:43 +0600

    Create Common Functions

--- cypress/integration/rokomari-test/rokomari-web.test.js
+++ cypress/integration/rokomari-test/rokomari-web.test.js
@@ -1,56 +1,58 @@
 /// <reference types="cypress" />
 
+// Import Data
+const rokomariData = require('./rokomariData');
+
+// Common Functions
+const signIn = () => {
+    cy.contains('Sign in').click();
+    cy.url().should("include", rokomariData.loginURL);
+    cy.get('form').within(() => {
+        cy.get('#j_username').type(rokomariData.username, { force: true });
+        cy.get('#j_password').type(rokomariData.password, { force: true });
+        cy.contains('Sign In').click({ force: true });
+    });
+};
+
 describe('Rokomari Test', () => {
     beforeEach(() => {
         Cypress.on('uncaught:exception', (err, runnable) => {
             // returning false here prevents Cypress from failing the test
             return false
         });
-        cy.visit('https://www.rokomari.com/book');
+        cy.visit(rokomariData.homeURL);
     });
 
     // Home Page Test
     it('Home Page Test', () => {
-        cy.url().should("eq", "https://www.rokomari.com/book");
+        cy.url().should("eq", rokomariData.homeURL);
     });
 
     // Sign In Test
     it('Sign In Test', () => {
-        cy.contains('Sign in').click();
-        cy.url().should("include", "https://www.rokomari.com/login");
-        cy.get('form').within(() => {
-            cy.get('#j_username').type('01688329403', { force: true });
-            cy.get('#j_password').type('rifat123', { force: true });
-            cy.contains('Sign In').click({ force: true });
-        });
+        signIn();
     });
 
     // Profile Test
     it('Profile Test', () => {
         // Sign In First
-        cy.contains('Sign in').click();
-        cy.url().should("include", "https://www.rokomari.com/login");
-        cy.get('form').within(() => {
-            cy.get('#j_username').type('01688329403', { force: true });
-            cy.get('#j_password').type('rifat123', { force: true });
-            cy.contains('Sign In').click({ force: true });
-        });
+        signIn();
 
         // 1. My Account
         cy.get(".user-menu").click();
         cy.contains("My Account").click();
-        cy.url().should("eq", "https://www.rokomari.com/my-section/profile");
+        cy.url().should("eq", rokomariData.profileURL);
         cy.contains("Change Information").click({ force: true });
 
         // 2. My Orders
         cy.get(".user-menu").click();
         cy.contains("My Orders").click();
-        cy.url().should("eq", "https://www.rokomari.com/my-section/orders");
+        cy.url().should("eq", rokomariData.myOrdersURL);
 
         // 3. My list
         cy.get(".user-menu").click();
         cy.contains("My List").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/my-section/list");
+        cy.url().should("eq", rokomariData.myListURL);
         cy.contains("Create New List").click({ force: true });
         cy.get('form').within(() => {
             cy.get('#list_title').type('iPhone', { force: true });
@@ -59,50 +61,36 @@ describe('Rokomari Test', () => {
             cy.get('#products')
                 .type('a', { force: true })
                 .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
                 .type('{enter}')
 
             cy.get('#products')
                 .type('a', { force: true })
                 .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
-                .type('{downarrow}')
                 .type('{enter}')
         });
         cy.get('#submit').click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/my-section/list");
+        cy.url().should("eq", rokomariData.myListURL);
 
         // 4. My wishlist
         cy.get(".user-menu").click();
         cy.contains("My Wishlist").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/my-section/wish-list");
+        cy.url().should("eq", rokomariData.myWishListURL);
 
         // 5. My rating review
         cy.get(".user-menu").click();
         cy.contains("My Rating & Reviews").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/my-section/reviews/not-reviewed");
+        cy.url().should("eq", rokomariData.myRatingReviewURL);
 
         // 6. My points
         cy.get(".user-menu").click();
         cy.contains("My Points").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/my-section/point");
+        cy.url().should("eq", rokomariData.myPointsURL);
 
         // 7. Sign out
         cy.get(".user-menu").click();
         cy.log(cy.contains("Sign Out"));
-        cy.visit("https://www.rokomari.com/logout");
-        cy.url().should("eq", "https://www.rokomari.com/book");
+        cy.visit(rokomariData.signOutURL);
+        cy.url().should("eq", rokomariData.homeURL);
     });
 
     // Search Test
@@ -114,13 +102,7 @@ describe('Rokomari Test', () => {
     // Notification Test
     it('Notification Test', () => {
         // Sign In First
-        cy.contains('Sign in').click();
-        cy.url().should("include", "https://www.rokomari.com/login");
-        cy.get('form').within(() => {
-            cy.get('#j_username').type('01688329403', { force: true });
-            cy.get('#j_password').type('rifat123', { force: true });
-            cy.contains('Sign In').click({ force: true });
-        });
+        signIn();
 
         cy.get("#unread-notification").click({ force: true });
         cy.contains("Notifications").should('exist');
@@ -129,38 +111,38 @@ describe('Rokomari Test', () => {
     // Cart Test
     it('Cart Test', () => {
         cy.get("#cart-icon").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/cart");
+        cy.url().should("eq", rokomariData.myCartURL);
     });
 
     // Top Bar
     it('Top Bar Test', () => {
         // 1. Book
         cy.contains("বই").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/book?ref=nm");
+        cy.url().should("eq", rokomariData.bookURL);
 
         // 2. Electronics
         cy.contains("ইলেক্ট্রনিক্স").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/electronics?ref=nm");
+        cy.url().should("eq", rokomariData.electronicsURL);
 
         // 3. Stationary and others
         cy.contains("স্টেশনারী ও অন্যান্য").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/stationary?ref=nm");
+        cy.url().should("eq", rokomariData.stationaryURL);
 
         // 4. Gift Finder
         cy.contains("গিফট ফাইন্ডার").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/giftfinder?ref=nm");
+        cy.url().should("eq", rokomariData.giftFinderURL);
 
         // 5. Institutional order
         cy.contains("প্রাতিষ্ঠানিক অর্ডার").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/corporate");
+        cy.url().should("eq", rokomariData.institutionalURL);
 
         // 6. Offers
         cy.contains("অফার সমূহ").click({ force: true });
-        cy.url().should("eq", "https://www.rokomari.com/offer");
+        cy.url().should("eq", rokomariData.offersURL);
 
         // 7. Blog
         cy.contains("ব্লগ").click({ force: true });
-        cy.visit("https://blog.rokomari.com/?ref=nm");
-        cy.url().should("eq", "https://blog.rokomari.com/?ref=nm");
+        cy.visit(rokomariData.blogURL);
+        cy.url().should("eq", rokomariData.blogURL);
     });
 });
---
+++ cypress/integration/rokomari-test/rokomariData.js
@@ -0,0 +1,23 @@
+const rokomariData = {
+    username: '01688329403',
+    password: 'rifat123',
+    homeURL: 'https://www.rokomari.com/book',
+    loginURL: 'https://www.rokomari.com/login',
+    profileURL: 'https://www.rokomari.com/my-section/profile',
+    myOrdersURL: 'https://www.rokomari.com/my-section/orders',
+    myListURL: 'https://www.rokomari.com/my-section/list',
+    myWishListURL: 'https://www.rokomari.com/my-section/wish-list',
+    myRatingReviewURL: 'https://www.rokomari.com/my-section/reviews/not-reviewed',
+    myPointsURL: 'https://www.rokomari.com/my-section/point',
+    signOutURL: 'https://www.rokomari.com/logout',
+    myCartURL: 'https://www.rokomari.com/cart',
+    bookURL: 'https://www.rokomari.com/book?ref=nm',
+    electronicsURL: 'https://www.rokomari.com/electronics?ref=nm',
+    stationaryURL: 'https://www.rokomari.com/stationary?ref=nm',
+    giftFinderURL: 'https://www.rokomari.com/giftfinder?ref=nm',
+    institutionalURL: 'https://www.rokomari.com/corporate',
+    offersURL: 'https://www.rokomari.com/offer',
+    blogURL: 'https://blog.rokomari.com/?ref=nm',
+};
+
+module.exports = rokomariData;
